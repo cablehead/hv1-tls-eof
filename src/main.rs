917c3f5122ee6d1131a8818485b232a2cadab346
Andy Gayton <andy@thecablelounge.com>
2023-11-18 21:30:06 -0500

    repo with http2

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ use std::net::SocketAddr;
 
 use http_body_util::Full;
 use hyper::body::Bytes;
-use hyper::server::conn::http1;
+use hyper::server::conn::http2;
 use hyper::service::service_fn;
 use hyper::{Request, Response};
 use hyper_util::rt::TokioIo;
+use hyper_util::rt::tokio::TokioExecutor;
 
 async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
     Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
@@ -34,7 +35,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         let io = TokioIo::new(stream);
 
         tokio::task::spawn(async move {
-            if let Err(err) = http1::Builder::new()
+            if let Err(err) = http2::Builder::new(TokioExecutor::new())
                 .serve_connection(io, service_fn(hello))
                 .await
             {
