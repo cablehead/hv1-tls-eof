faba33f14dc564c018966cc47a21cb3d98f76921
Andy Gayton <andy@thecablelounge.com>
2023-11-20 16:50:20 -0500

    enable tracing

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,9 @@ use std::net::SocketAddr;
 
 use http_body_util::Full;
 use hyper::body::Bytes;
-use hyper::server::conn::http2;
 use hyper::service::service_fn;
 use hyper::{Request, Response};
 use hyper_util::rt::TokioIo;
-use hyper_util::rt::tokio::TokioExecutor;
 
 async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
     Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
@@ -15,15 +13,16 @@ async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let subscriber = tracing_subscriber::FmtSubscriber::builder()
+        .with_max_level(tracing::Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-
     let accept_tls = configure_tls(PEM);
-
     let listener = tokio::net::TcpListener::bind(addr).await?;
-
     loop {
         let (stream, _) = listener.accept().await?;
-
         let stream = match accept_tls.accept(stream).await {
             Ok(s) => s,
             Err(e) => {
@@ -33,11 +32,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         };
 
         let io = TokioIo::new(stream);
-
         tokio::task::spawn(async move {
-            if let Err(err) = http2::Builder::new(TokioExecutor::new())
-                .serve_connection(io, service_fn(hello))
-                .await
+            if let Err(err) = hyper::server::conn::http2::Builder::new(
+                hyper_util::rt::tokio::TokioExecutor::new(),
+            )
+            .serve_connection(io, service_fn(hello))
+            .await
             {
                 println!("Error serving connection: {:?}", err);
             }
